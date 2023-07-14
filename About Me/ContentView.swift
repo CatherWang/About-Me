d7fae70a0c1c190beb863ee7b7c983204efd3393
Catherine Wang <catheri15432077@gmail.com>
2023-07-14 07:28:08 -0700

    Initial Commit

---
+++ About Me/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  About Me
+//
+//  Created by CatherineWang on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ State private var facts = ""
+    var body: some View {
+        VStack{
+            
+            Text("Hi, I'm Catherine!")
+                .font(.title)
+            Image("IMG_6725")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .border(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                .padding(10)
+                .cornerRadius(40)
+            Button("Learn more about me") {
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }
+    }
+    
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
